a9d0d78f58fd7bed29ea422a99895a0b220007da
yuranius <yuranius.webber@gmail.com>
2022-03-27 13:45:13 +0300

    27/03

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import logo from "./image/many.webp";
 import "./App.css";
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
           <div className="conteiner">
             <div className='header__body'>
               <a href='#' className='header__logo'>
-                <img src='logo' alt=''></img>
+                <img src={logo} alt='logo' />
               </a>
               <div className='header__burger'>
                 <span></span>
@@ -58,36 +58,65 @@ function App() {
           <div className="conteiner">
 					  <div className='main__body'>
               <div className="sidebar">
-                <h2 className='sidebar__title'>
-                  Posts
-                </h2>
-                <div className='sidebar__text'>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
-                  totam cumque ea debitis, quasi nesciunt magnam fugit reprehenderit
-                  nihil. Impedit, quo praesentium pariatur atque veniam unde
-                  aspernatur optio quaerat facere.
+                <div className="sidebar__block">
+                  <nav className='sidebar__menu'>
+                  <ul className='sidebar__list'>
+                    <li>
+                      <a href='' className='sidebar__link'>Главное</a>
+                    </li>
+                    <li>
+                      <a href='' className='sidebar__link'>Уроки</a>
+                    </li>
+                    <li>
+                      <a href='' className='sidebar__link'>
+                        Контакты
+                      </a>
+                    </li>
+                    <li>
+                      <a href='' className='sidebar__link'>
+                        Шпаргалки
+                      </a>
+                    </li>
+                    <li>
+                      <a href='' className='sidebar__link'>
+                        Полезное
+                      </a>
+                    </li>
+                    <li>
+                      <a href='' className='sidebar__link'>
+                        О канале
+                      </a>
+                    </li>
+                  </ul>
+                  </nav>
                 </div>
               </div>
               <div className="posts">
-                <h2 className='posts__title' id='two-title'>
-                  Posts
-                </h2>
-                <div className='posts__text' id='two-text'>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
-                  totam cumque ea debitis, quasi nesciunt magnam fugit reprehenderit
-                  nihil. Impedit, quo praesentium pariatur atque veniam unde
-                  aspernatur optio quaerat facere.
+                <div className="posts__block">
+                  <h2 className='posts__title'>
+                    Posts
+                    
+                  </h2>
+                  <div className='posts__text'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+                    totam cumque ea debitis, quasi nesciunt magnam fugit reprehenderit
+                    nihil. Impedit, quo praesentium pariatur atque veniam unde
+                    aspernatur optio quaerat facere.
+                  </div>
                 </div>
               </div>
               <div className="aside">
-                <h2 className='posts__title' id='two-title'>
-                  Posts
-                </h2>
-                <div className='posts__text' id='two-text'>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
-                  totam cumque ea debitis, quasi nesciunt magnam fugit reprehenderit
-                  nihil. Impedit, quo praesentium pariatur atque veniam unde
-                  aspernatur optio quaerat facere.
+                <div className="aside__block">
+                  <h2 className='aside__title'>
+                    {/* <img src={many} alt=""/> */}
+                    Posts
+                  </h2>
+                  <div className='aside__text'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+                    totam cumque ea debitis, quasi nesciunt magnam fugit reprehenderit
+                    nihil. Impedit, quo praesentium pariatur atque veniam unde
+                    aspernatur optio quaerat facere.
+                  </div>
                 </div>
               </div>
 					  </div>
@@ -99,7 +128,7 @@ function App() {
 				<footer className='footer'>
         <div className="conteiner">
 					<div className='footer__body'>
-						<h2 className='footer__title' id='two-title'>
+						<h2 className='footer__title'>
 							футер
 						</h2>
 					</div>
