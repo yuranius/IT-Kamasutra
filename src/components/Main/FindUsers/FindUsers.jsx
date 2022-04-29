8c906fff74eb0b8ac7d45bcc16b10b3710f8a45c
yuranius <yuranius.webber@gmail.com>
2022-04-29 22:02:12 +0300

    29/04 get beta API users samuraijs

--- src/components/Main/FindUsers/FindUsers.jsx
+++ src/components/Main/FindUsers/FindUsers.jsx
@@ -1,101 +1,108 @@
+import * as axios from "axios";
 import React from "react";
 import scss from "./FindUsers.module.scss";
 import UserItem from "./UserItem/UserItem";
+import userPhoto from './../../../image/user-img.webp'
 
 let FindUsers = (props) => {
    if (props.users.length === 0) {
-      props.setUsers([
-         {
-            id: 1,
-            followed: false,
-            avatar: "http://sociala.uitheme.net/assets/images/user-7.png",
-            name: "Victor Exrixon ",
-            user: "@macale343",
-         },
-         {
-            id: 2,
-            followed: true,
-            avatar: "http://sociala.uitheme.net/assets/images/user-12.png",
-            name: "Surfiya Zakir ",
-            user: "@macale343",
-         },
-         {
-            id: 3,
-            followed: false,
-            avatar: "http://sociala.uitheme.net/assets/images/user-4.png",
-            name: "Goria Coast ",
-            user: "@macale343",
-         },
-         {
-            id: 4,
-            followed: true,
-            avatar: "http://sociala.uitheme.net/assets/images/user-3.png",
-            name: "Hurin Seary ",
-            user: "@macale343",
-         },
-         {
-            id: 5,
-            followed: false,
-            avatar: "http://sociala.uitheme.net/assets/images/user-2.png",
-            name: "Victor Exrixon ",
-            user: "@macale343",
-         },
-         {
-            id: 6,
-            followed: true,
-            avatar: "http://sociala.uitheme.net/assets/images/user-8.png",
-            name: "Surfiya Zakir ",
-            user: "@macale343",
-         },
-         {
-            id: 7,
-            followed: false,
-            avatar: "http://sociala.uitheme.net/assets/images/user-4.png",
-            name: "Goria Coast ",
-            user: "@macale343",
-         },
-         {
-            id: 8,
-            followed: false,
-            avatar: "http://sociala.uitheme.net/assets/images/user-11.png",
-            name: "Hurin Seary ",
-            user: "@macale343",
-         },
-         {
-            id: 9,
-            followed: false,
-            avatar: "http://sociala.uitheme.net/assets/images/user-8.png",
-            name: "Surfiya Zakir ",
-            user: "@macale343",
-         },
-         {
-            id: 10,
-            followed: false,
-            avatar: "http://sociala.uitheme.net/assets/images/user-3.png",
-            name: "Goria Coast ",
-            user: "@macale343",
-         },
-         {
-            id: 11,
-            followed: false,
-            avatar: "http://sociala.uitheme.net/assets/images/user-4.png",
-            name: "Hurin Seary ",
-            user: "@macale343",
-         },
-         {
-            id: 12,
-            followed: false,
-            avatar: "http://sociala.uitheme.net/assets/images/user-7.png",
-            name: "Aliqa Macale ",
-            user: "@macale343",
-         },
-      ]);
+
+      axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+         props.setUsers(response.data.items)
+      })
+
+      // props.setUsers([
+      //    {
+      //       id: 1,
+      //       followed: false,
+      //       avatar: "http://sociala.uitheme.net/assets/images/user-7.png",
+      //       name: "Victor Exrixon ",
+      //       user: "@macale343",
+      //    },
+      //    {
+      //       id: 2,
+      //       followed: true,
+      //       avatar: "http://sociala.uitheme.net/assets/images/user-12.png",
+      //       name: "Surfiya Zakir ",
+      //       user: "@macale343",
+      //    },
+      //    {
+      //       id: 3,
+      //       followed: false,
+      //       avatar: "http://sociala.uitheme.net/assets/images/user-4.png",
+      //       name: "Goria Coast ",
+      //       user: "@macale343",
+      //    },
+      //    {
+      //       id: 4,
+      //       followed: true,
+      //       avatar: "http://sociala.uitheme.net/assets/images/user-3.png",
+      //       name: "Hurin Seary ",
+      //       user: "@macale343",
+      //    },
+      //    {
+      //       id: 5,
+      //       followed: false,
+      //       avatar: "http://sociala.uitheme.net/assets/images/user-2.png",
+      //       name: "Victor Exrixon ",
+      //       user: "@macale343",
+      //    },
+      //    {
+      //       id: 6,
+      //       followed: true,
+      //       avatar: "http://sociala.uitheme.net/assets/images/user-8.png",
+      //       name: "Surfiya Zakir ",
+      //       user: "@macale343",
+      //    },
+      //    {
+      //       id: 7,
+      //       followed: false,
+      //       avatar: "http://sociala.uitheme.net/assets/images/user-4.png",
+      //       name: "Goria Coast ",
+      //       user: "@macale343",
+      //    },
+      //    {
+      //       id: 8,
+      //       followed: false,
+      //       avatar: "http://sociala.uitheme.net/assets/images/user-11.png",
+      //       name: "Hurin Seary ",
+      //       user: "@macale343",
+      //    },
+      //    {
+      //       id: 9,
+      //       followed: false,
+      //       avatar: "http://sociala.uitheme.net/assets/images/user-8.png",
+      //       name: "Surfiya Zakir ",
+      //       user: "@macale343",
+      //    },
+      //    {
+      //       id: 10,
+      //       followed: false,
+      //       avatar: "http://sociala.uitheme.net/assets/images/user-3.png",
+      //       name: "Goria Coast ",
+      //       user: "@macale343",
+      //    },
+      //    {
+      //       id: 11,
+      //       followed: false,
+      //       avatar: "http://sociala.uitheme.net/assets/images/user-4.png",
+      //       name: "Hurin Seary ",
+      //       user: "@macale343",
+      //    },
+      //    {
+      //       id: 12,
+      //       followed: false,
+      //       avatar: "http://sociala.uitheme.net/assets/images/user-7.png",
+      //       name: "Aliqa Macale ",
+      //       user: "@macale343",
+      //    },
+      // ]);
    }
    let userElements = props.users.map((u) => (
       <UserItem
          id={u.id}
          key={u.id.toString()}
-         avatar={u.avatar}
+         avatar={u.photos.small != null ? u.photos.small : userPhoto} //? если small не равен null, тогда берем small, иначе userPhoto
          name={u.name}
          user={u.user}
          followed={u.followed}
