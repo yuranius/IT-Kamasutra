769b34db4f385f4fbe2337a4f1ba82cac454a9a1
yuranius <yuranius.webber@gmail.com>
2022-06-06 09:32:52 +0300

    06/06 fixed error paginator

--- src/components/Common/Paginator/Paginator.jsx
+++ src/components/Common/Paginator/Paginator.jsx
@@ -1,46 +1,39 @@
-import React from "react";
-import scss from "./Paginator.module.scss";
-
+import React from 'react'
+import scss from './Paginator.module.scss'
 
 let Paginator = (props) => {
-	console.log('📢 [Paginator.jsx:6]', props);
 
-	let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+  let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
 
-	let pages = [];
- 
-	for (let index = 1; index <= pagesCount; index++) {
-	   pages.push(index);
-	}
+  let pages = []
 
+  for (let index = 1; index <= pagesCount; index++) {
+    pages.push(index)
+  }
 
-	// let pagesElement = pages.map( ( p ) => (
-	// 	<FindUsersPages 
-	// 	id = { p }
-	// 	key = { p.toString() } 
-	// 	onPageChanged= { props.onPageChanged }
-	// 	currentPage = { props.currentPage }
-	// 	/>
-	//  ));
+  return (
+    <>
+      {pages.map((p) => {
+		  
+        return (
+			
+          <span
+            key={p}
+            onClick={() => {
+              props.onPageChanged(p)
+            }}
+            className={
+              props.currentPage === p
+                ? scss['namber-page__page-active']
+                : scss['namber-page__page']
+            }
+          >
+            {p}
+          </span>
+        )
+      })}
+    </>
+  )
+}
 
-   return (
-	   <> 
-		{ pages.map( ( p ) => {
-			return (
-		<span
-			key={p.id}
-			onClick={() => {
-				props.onPageChanged(p.id);
-			}}
-			className={props.currentPage === p.id ? scss["namber-page__page-active"] : scss["namber-page__page"]}
-		>
-			{p.id}
-		</span>
-			)})
-		}
-		</>
-   )
-
-};
-
-export default Paginator;
+export default Paginator
